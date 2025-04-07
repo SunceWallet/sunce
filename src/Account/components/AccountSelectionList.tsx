662c262591624f78d016f72e172c6493772a8f1a
Kosta Korenkov <kosta@korenkov.net>
2025-04-07 15:05:30 +0200

    hide hidden accounts on SEP07 account selection screen

--- src/Account/components/AccountSelectionList.tsx
+++ src/Account/components/AccountSelectionList.tsx
@@ -90,7 +90,7 @@ const AccountSelectionListItem = React.memo(
           primary={props.account.name}
           secondary={
             <React.Suspense fallback={<InlineLoader />}>
-              <AccountBalances publicKey={props.account.accountID} testnet={props.account.testnet} />
+              <AccountBalances publicKey={props.account.accountID} testnet={props.account.testnet} showHidden={false} />
             </React.Suspense>
           }
         />
