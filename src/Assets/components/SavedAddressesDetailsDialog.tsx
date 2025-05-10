167a9546d1a5b2e83964eb895c01a553456bb245
Kosta Korenkov <kosta@korenkov.net>
2025-05-10 11:30:13 +0200

    feat: show QR code on Saved Address page (#137)
    With ability to copy to clipboard

--- src/Assets/components/SavedAddressesDetailsDialog.tsx
+++ src/Assets/components/SavedAddressesDetailsDialog.tsx
@@ -1,13 +1,16 @@
-import React from "react"
-import { useTranslation } from "react-i18next"
-import MainTitle from "~Generic/components/MainTitle"
-import DialogBody from "~Layout/components/DialogBody"
 import { TextField } from "@material-ui/core"
 import CloseIcon from "@material-ui/icons/Close"
-import { useForm } from "react-hook-form"
 import { nanoid } from "nanoid"
-import { isMuxedAddress, isPublicKey, isStellarAddress } from "~Generic/lib/stellar-address"
+import QRCode from "qrcode.react"
+import React, { useCallback, useState } from "react"
+import { useForm } from "react-hook-form"
+import { useTranslation } from "react-i18next"
 import { ActionButton, DialogActionsBox } from "~Generic/components/DialogActions"
+import MainTitle from "~Generic/components/MainTitle"
+import { useClipboard } from "~Generic/hooks/userinterface"
+import { isMuxedAddress, isPublicKey, isStellarAddress } from "~Generic/lib/stellar-address"
+import { Box } from "~Layout/components/Box"
+import DialogBody from "~Layout/components/DialogBody"
 
 export interface SavedAddressDetailsDialogProps {
   address?: string
@@ -22,8 +25,15 @@ export interface SavedAddressValues {
   label: string
 }
 
+const isValidAddress = (value: string) => isPublicKey(value) || isMuxedAddress(value) || isStellarAddress(value)
+
 function SavedAddressDetailsDialog(props: SavedAddressDetailsDialogProps) {
   const { t } = useTranslation()
+
+  const clipboard = useClipboard()
+  const copyToClipboard = React.useCallback(() =>
+    clipboard.copyToClipboard(props.address || ""), [clipboard, props.address]
+  )
 
   const form = useForm<SavedAddressValues>({
     defaultValues: {
@@ -32,17 +42,28 @@ function SavedAddressDetailsDialog(props: SavedAddressDetailsDialogProps) {
     }
   })
 
+  const [qrCodeAddress, setQRCodeAddress] = useState<string | undefined>(props.address)
+
   const formID = React.useMemo(() => nanoid(), [])
 
-  const handleFormSubmission = () => {
+  const handleFormSubmission = useCallback(() => {
     const values = form.getValues()
     if (!values.address) return
     props.onSave(values.address, values.label)
-  }
+  }, [form, props.onSave])
+
+  const handleFormChange = useCallback(() => {
+    console.log(isValidAddress(form.getValues().address))
+    if (isValidAddress(form.getValues().address)) {
+      setQRCodeAddress(form.getValues().address)
+    } else {
+      setQRCodeAddress(undefined)
+    }
+  }, [props.address, form.getValues().address])
 
   return (
     <DialogBody excessWidth={12} top={<MainTitle onBack={props.onClose} title={props.label} />}>
-      <form id={formID} noValidate onSubmit={form.handleSubmit(handleFormSubmission)}>
+      <form id={formID} noValidate onChange={handleFormChange} onSubmit={form.handleSubmit(handleFormSubmission)}>
         <TextField
           error={Boolean(form.errors.address)}
           fullWidth
@@ -52,9 +73,7 @@ function SavedAddressDetailsDialog(props: SavedAddressDetailsDialogProps) {
           inputRef={form.register({
             required: t<string>("account.saved-address-details.validation.no-address"),
             validate: value =>
-              isPublicKey(value) ||
-              isMuxedAddress(value) ||
-              isStellarAddress(value) ||
+              isValidAddress(value) ||
               t<string>("account.saved-address-details.validation.invalid-address")
           })}
           label={form.errors.address ? form.errors.address.message : t("account.saved-address-details.address.label")}
@@ -81,6 +100,11 @@ function SavedAddressDetailsDialog(props: SavedAddressDetailsDialogProps) {
           }}
           placeholder={t("account.saved-address-details.label.label")}
         />
+        {qrCodeAddress && (
+          <Box onClick={copyToClipboard} textAlign="center" width="100%" margin="32px auto" style={{ cursor: "pointer" }}>
+            <QRCode value={qrCodeAddress} size={192} />
+          </Box>
+        )}
         <DialogActionsBox desktopStyle={{ marginTop: 64 }}>
           {props.address && (
             <ActionButton
